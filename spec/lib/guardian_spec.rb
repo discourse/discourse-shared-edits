d3047133a296c053f2410c322f045a8bdea93451
Sam <sam.saffron@gmail.com>
2022-12-30 07:56:51 +1100

    FEATURE: allow TL4 to toggle shared edits
    also fixes  jumping to end of post when cursor is at the start

---
+++ spec/lib/guardian_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+describe ::Guardian do
+  fab!(:moderator) { Fabricate(:moderator) }
+  fab!(:user) { Fabricate(:user) }
+
+  it "disallows shared edits from anon" do
+    expect(Guardian.new.can_toggle_shared_edits?).to eq(false)
+  end
+
+  it "disallows shared edits for tl3 users" do
+    user.trust_level = 3
+    expect(Guardian.new(user).can_toggle_shared_edits?).to eq(false)
+  end
+
+  it "allows shared edits for staff" do
+    expect(Guardian.new(moderator).can_toggle_shared_edits?).to eq(true)
+  end
+
+  it "allows shared edits for tl4" do
+    user.trust_level = 4
+    expect(Guardian.new(user).can_toggle_shared_edits?).to eq(true)
+  end
+end
