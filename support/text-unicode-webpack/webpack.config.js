fbf4d4d957f1e355e37421d0dc3c2e77ca2eaaea
Jarek Radosz <jradosz@gmail.com>
2024-03-10 22:52:04 +0100

    this entry is the default

--- support/text-unicode-webpack/webpack.config.js
+++ support/text-unicode-webpack/webpack.config.js
@@ -1,7 +1,6 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
   mode: "production",
   output: {
     path: path.resolve(__dirname, "..", "..", "public", "javascripts"),
