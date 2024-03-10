a61b1d4010654e632f7a9289aa1a68e165937a6c
Jarek Radosz <jradosz@gmail.com>
2024-03-10 22:10:10 +0100

    Build straight into /public

--- support/text-unicode-webpack/webpack.config.js
+++ support/text-unicode-webpack/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './src/index.js',
   mode: 'production',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '..', '..', 'public', 'javascripts'),
     filename: 'text-unicode-dist.js',
     library: 'otLib',
     libraryTarget: 'umd'
