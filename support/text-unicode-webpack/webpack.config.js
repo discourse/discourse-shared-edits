71f33628397fe0105b737cebe4829b76e6448a87
Jarek Radosz <jradosz@gmail.com>
2024-03-10 22:34:37 +0100

    format

--- support/text-unicode-webpack/webpack.config.js
+++ support/text-unicode-webpack/webpack.config.js
@@ -1,20 +1,20 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
-  mode: 'production',
+  entry: "./src/index.js",
+  mode: "production",
   output: {
-    path: path.resolve(__dirname, '..', '..', 'public', 'javascripts'),
-    filename: 'text-unicode-dist.js',
-    library: 'otLib',
-    libraryTarget: 'umd'
+    path: path.resolve(__dirname, "..", "..", "public", "javascripts"),
+    filename: "text-unicode-dist.js",
+    library: "otLib",
+    libraryTarget: "umd",
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        use: [{ loader: 'babel-loader'}]
-      }
-    ]
-  }
+        use: [{ loader: "babel-loader" }],
+      },
+    ],
+  },
 };
