4b09780694a4db96c707391f41be9c2f26ad400a
Jarek Radosz <jradosz@gmail.com>
2024-10-26 21:31:24 +0200

    DEV: Add a system spec

---
+++ spec/system/edit_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+RSpec.describe "Discourse Shared Edits | Editing a post", system: true do
+  fab!(:admin)
+  fab!(:post) { Fabricate(:post, user: admin, raw: "lorem ipsum\n") }
+  let(:composer) { PageObjects::Components::Composer.new }
+
+  before { sign_in(admin) }
+
+  it "allows the user to edit and save a post" do
+    visit(post.topic.relative_url)
+
+    find(".show-more-actions").click
+    find(".show-post-admin-menu").click
+    find(".admin-toggle-shared-edits").click
+
+    try_until_success do
+      revision = SharedEditRevision.find_by(post_id: post.id, version: 1)
+      expect(revision).to be_present
+      expect(revision.raw).to eq("lorem ipsum\n")
+      expect(revision.revision).to eq("[]")
+    end
+
+    find(".shared-edit").click
+    expect(composer).to have_content("lorem ipsum")
+
+    composer.type_content "foo"
+    try_until_success do
+      revision = SharedEditRevision.find_by(post_id: post.id, version: 2)
+      expect(revision).to be_present
+      expect(revision.revision).to eq("[12,\"foo\"]")
+    end
+
+    composer.type_content " bar"
+    try_until_success do
+      revision = SharedEditRevision.find_by(post_id: post.id, version: 3)
+      expect(revision).to be_present
+      expect(revision.revision).to eq("[15,\" bar\"]")
+    end
+
+    find(".leave-shared-edit .btn-primary").click
+    try_until_success do
+      expect(post.reload.raw).to eq("lorem ipsum\nfoo bar")
+      expect(find("#post_1 .cooked > p")).to have_content("lorem ipsum\nfoo bar")
+    end
+  end
+end
